d75783c9a0d0738b9a9cdc9530953f17269f5f57
Leonardo <106412608+leshyG@users.noreply.github.com>
2025-11-13 11:52:30 -0500

    Función calculateOperation que muestra el resultado en pantalla y que funciona según el id

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let operators = Array.from(document.querySelectorAll(".ops"));
 
 let calculate = document.getElementById("equal");
 let clear = document.getElementById("clear");
-
 function add(a, b) {
     console.log(`a: ${a} b:${b}`)
     return a + b;
@@ -24,4 +23,24 @@ function multiply(a, b) {
 function divide(a, b) {
     console.log(`a: ${a} b:${b}`)
     return a / b;
+}
+
+function calculateOperation(a, b, id) {
+    let result = 0;
+    switch (id) {
+        case "addition":
+            result = add(a, b);
+            break;
+        case "subtract":
+            result = subtract(a, b);
+            break;
+        case "multiply":
+            result = multiply(a, b);
+            break;
+        case "divide":
+            result = divide(a, b);
+            break;
+    }
+    displayScreen.textContent = result;
+    return result;
 }
