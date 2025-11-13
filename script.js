61ed65157179f7ba50840374259139cad5106864
Leonardo <106412608+leshyG@users.noreply.github.com>
2025-11-13 11:48:40 -0500

    Variables asignadas a cada uno de los elementos HTML de la calculadora

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let displayScreen = document.querySelector(".operands");
+
+let numbers = Array.from(document.querySelectorAll(".btn"));
+let operators = Array.from(document.querySelectorAll(".ops"));
+
+let calculate = document.getElementById("equal");
+let clear = document.getElementById("clear");
+
 function add(a, b) {
     console.log(`a: ${a} b:${b}`)
     return a + b;
